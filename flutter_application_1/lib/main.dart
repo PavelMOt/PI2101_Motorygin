8c725963e1238c0812b026b67d8649581e6e61b9
PavelMot <motoryginpavel274@gmail.com>
2024-03-24 23:40:54 +0300

    change buttons

--- flutter_application_1/lib/main.dart
+++ flutter_application_1/lib/main.dart
@@ -7,7 +7,7 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -25,7 +25,7 @@ class MyApp extends StatelessWidget {
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
 
-  
+
 
   final String title;
 
@@ -36,44 +36,71 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
 
-  void _incrementCounter() {
+  void _incrementCounterAdd() {
     setState(() {
    
       _counter++;
     });
   }
-
+  void _incrementCounterRemove() {
+    setState(() {
+   
+      _counter--;
+    });
+  }
+ 
+  
   @override
   Widget build(BuildContext context) {
-   
+  
     return Scaffold(
       appBar: AppBar(
-       
+    
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        
+       
         title: Text(widget.title),
       ),
       body: Center(
        
         child: Column(
-          
+       
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             const Text(
-              'You have pushed the button this many times:',
+              'Значение инкремента:',
             ),
             Text(
               '$_counter',
               style: Theme.of(context).textTheme.headlineMedium,
-            ),
+            ), Row(mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            ElevatedButton(
+onPressed:_incrementCounterAdd,
+style: ElevatedButton.styleFrom(
+  backgroundColor:  Colors.green,
+shape: RoundedRectangleBorder(
+borderRadius: BorderRadius.circular(12), // <— Radius
+),
+),
+child: const Icon(Icons.add),
+),
+        ElevatedButton(
+onPressed:_incrementCounterRemove,
+
+style: ElevatedButton.styleFrom(
+  backgroundColor:  Colors.red,
+shape: RoundedRectangleBorder(
+borderRadius: BorderRadius.circular(12), // <— Radius
+),
+),
+child: const Icon(Icons.remove),
+),
+          ],),
           ],
         ),
+        
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+     
     );
   }
 }
