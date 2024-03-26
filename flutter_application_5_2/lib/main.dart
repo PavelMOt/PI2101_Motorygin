79f02e88d8625fd3488a2f73e8d9c6f79c618460
PavelMot <motoryginpavel274@gmail.com>
2024-03-26 20:24:08 +0300

    add list 2

--- flutter_application_5_2/lib/main.dart
+++ flutter_application_5_2/lib/main.dart
@@ -2,16 +2,21 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(
       MaterialApp(
-          title: 'lab 4',
+          title: 'lab 5',
           debugShowCheckedModeBanner: false,
           home: Scaffold(
-            appBar: AppBar(
-              title: const Text(
-                'список элементов',
-                style: TextStyle(color: Colors.white),
+              appBar: AppBar(
+                title: const Text(
+                  'список элементов',
+                  style: TextStyle(color: Colors.white),
+                ),
+                backgroundColor: Colors.green,
               ),
-              backgroundColor: Colors.green,
-            ),
-            // ListView.builder
-          )), // MaterialApp
-    );
+              body: ListView.builder(
+                itemBuilder: (context, index) {
+                  return Text('строка $index');
+                },
+              ) // ListView.builder
+              )), // MaterialApp
+    );  // ListView.builder
+        
