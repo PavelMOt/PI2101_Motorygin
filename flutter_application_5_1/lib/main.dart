7185ebd757281303d246b1425130446c7367b75b
PavelMot <motoryginpavel274@gmail.com>
2024-03-26 20:17:12 +0300

    add list

--- flutter_application_5_1/lib/main.dart
+++ flutter_application_5_1/lib/main.dart
@@ -3,14 +3,20 @@ import 'package:flutter/material.dart';
 void main() => runApp(MaterialApp(
         debugShowCheckedModeBanner: false,
         home: Scaffold(
-          appBar: AppBar(
-            title: const Text(
-              'список элементов',
-              style: TextStyle(color: Colors.white),
+            appBar: AppBar(
+              title: const Text(
+                'список элементов',
+                style: TextStyle(color: Colors.white),
+              ),
+              backgroundColor: Colors.green,
             ),
-            backgroundColor: Colors.green,
-          ),
-          // ListView
-        ) // Scaffold
+            body: ListView(children: const [
+              Text('0000'),
+              Divider(),
+              Text('0001'),
+              Divider(),
+              Text('0010'),
+            ]) // ListView
+            ) // Scaffold
         ) // MaterialApp
     );
